88fab626b3b98363dc943978021880c8896d57f6
JoshuaSE-git <joshua.s.emralino@gmail.com>
2025-01-29 16:50:41 -0800

    first commit

---
+++ CISC190/Lab1/HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * This is my first java app. It prints out "Hello world!"
+ * to the console.
+ * @author Joshua Emralino, jemralino@student.sdccd.edu
+ * @since 1/28/2025 - (Joshua Emralino, v1.0)
+ * @version v1.0
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Joshua Emralino");
+    }
+}
