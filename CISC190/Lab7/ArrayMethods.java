d3fb9ab9bbbe7d0f180d13452a10043d43b6aa8e
Joshua Emralino <joshua.s.emralino@gmail.com>
2025-04-17 11:13:28 -0700

    190

--- CISC190/Lab7/ArrayMethods.java
+++ CISC190/Lab7/ArrayMethods.java
@@ -1,10 +1,22 @@
 import java.util.Arrays;
 
+/**
+ * Class with static methods that operate on Arrays.
+ *
+ * @author Joshua Emralino, jemralino@student.sdccd.edu
+ * @version v1.0
+ * @since 4/17/2025 - (Joshua Emralino v1.0)
+ */
 public class ArrayMethods {
 
-  private static int[] myArray = {7, 8, 8, 3, 4, 9, 8, 7};
+  private int[] myArray = {7, 8, 8, 3, 4, 9, 8, 7};
 
-  public static int count() {
+  /**
+   * Counts the number of elements in the array.
+   *
+   * @return number of elements
+   */
+  public int count() {
     int count = 0;
     for (int i : myArray) {
       count++;
@@ -12,7 +24,12 @@ public class ArrayMethods {
     return count;
   }
 
-  public static int sum() {
+  /**
+   * Sums the integers in the array and returns the sum.
+   *
+   * @return sum of the integer elements
+   */
+  public int sum() {
     int sum = 0;
     for (int i : myArray) {
       sum += i;
@@ -20,11 +37,21 @@ public class ArrayMethods {
     return sum;
   }
 
-  public static double average() {
+  /**
+   * Gets the average value of the array.
+   *
+   * @return average value in the array
+   */
+  public double average() {
     return (double) sum() / count();
   }
 
-  public static int findMax() {
+  /**
+   * Finds the max value in the array and returns it.
+   *
+   * @return max integer value in array
+   */
+  public int findMax() {
     int max = myArray[0];
     for (int i : myArray) {
       if (i > max) {
@@ -34,7 +61,12 @@ public class ArrayMethods {
     return max;
   }
 
-  public static int findIndexOfMax() {
+  /**
+   * Finds the max value in the array and returns its index.
+   *
+   * @return index of max integer value in array
+   */
+  public int findIndexOfMax() {
     int maxIndex = 0;
     int max = myArray[0];
     for (int i = 0; i < myArray.length; i++) {
@@ -46,7 +78,13 @@ public class ArrayMethods {
     return maxIndex;
   }
 
-  public static int findLast(int key) {
+  /**
+   * Finds the last occurrence of specified input and returns its index.
+   *
+   * @param key value to search for
+   * @return index of last occurrence of value
+   */
+  public int findLast(int key) {
     for (int i = myArray.length - 1; i >= 0; i--) {
       if (myArray[i] == key) {
         return i;
@@ -55,10 +93,16 @@ public class ArrayMethods {
     return -1;
   }
 
-  public static int[] findAll(int key) {
+  /**
+   * Finds all indexes of specified input and returns them in an array.
+   *
+   * @param key value to search for
+   * @return reference to array of indexes of specified input
+   */
+  public int[] findAll(int key) {
     int count = 0;
     for (int i : myArray) {
-      if (i == myArray[key]) {
+      if (i == key) {
         count++;
       }
     }
@@ -66,7 +110,7 @@ public class ArrayMethods {
     int[] retArr = new int[count];
     int retIndex = 0;
     for (int i = 0; i < myArray.length; i++) {
-      if (myArray[i] == myArray[key]) {
+      if (myArray[i] == key) {
         retArr[retIndex] = i;
         retIndex++;
       }
@@ -75,31 +119,53 @@ public class ArrayMethods {
     return retArr;
   }
 
-  public static int[] getArray() {
+  /**
+   * Getter for array field.
+   *
+   * @return reference for array field.
+   */
+  public int[] getArray() {
     return myArray;
   }
 
-  public static int[] copyArray() {
+  /**
+   * Returns a reference to the duplicated array field.
+   *
+   * @return reference to duplicated array
+   */
+  public int[] copyArray() {
     return Arrays.copyOf(myArray, myArray.length);
   }
 
-  public static void print(int[] a) {
+  /**
+   * Prints out the contents of the array.
+   *
+   * @param inArray the array to print out.
+   */
+  public void print(int[] inArray) {
+    System.out.print("{");
+    for (int i = 0; i < inArray.length - 1; i++) {
+      System.out.printf("%d,", inArray[i]);
+    }
+    if (inArray.length > 0) {
+      System.out.print(inArray[inArray.length - 1]);
+    }
+    System.out.println("}");
+  }
+
+  public void sortArray(int[] a) {
     return;
   }
 
-  public static void sortArray(int[] a) {
-    return;
-  }
-
-  public static int[] reverseArray(int[] a) {
+  public int[] reverseArray(int[] a) {
     return new int[0];
   }
 
-  public static void genFiveByFiveRows() {
+  public void genFiveByFiveRows() {
     return;
   }
 
-  public static void genFiveByFiveCols() {
+  public void genFiveByFiveCols() {
     return;
   }
 }
