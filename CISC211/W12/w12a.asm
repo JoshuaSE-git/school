99f3119ae20d7d98b9e00f894eee7f1314bbe133
Joshua Emralino <joshua.s.emralino@gmail.com>
2025-05-04 17:47:06 -0700

    assignent

--- CISC211/W12/w12a.asm
+++ CISC211/W12/w12a.asm
@@ -1,31 +1,62 @@
 section .text
   global _start
 
-_start:
-  mov eax, [num]
-  push eax
-  call _func
+_start:                     ;main function
+  push DWORD[num]
+  call _evenOrOdd
 
-_func:
+  mov eax, 1
+  int 0x80
+
+_evenOrOdd:                 ;determines even or odd and prints result
   push ebp
-  mov ebp, esp
+  mov ebp, esp              ;initializing stack frame
   sub esp, 4
-
   mov eax, DWORD[ebp+8]
   mov DWORD[ebp-4], eax
 
-  test eax, 1
+  test eax, 1               ;logic for even/odd
   jz even
   jmp odd
+
+even:                       ;push evenMsg pointer and evenLen value to stack (arguments)
+  push msgEven     
+  push evenLen
+  jmp continueEvenOrOdd     ;jmp back to next instruction in _evenOrOdd
+
+odd:                        ;push oddMsg pointer and oddLen value to stack (arguments)
+  push msgOdd
+  push oddLen
+  jmp continueEvenOrOdd     ;jmp back to next instruction in _evenOrOdd
+
+continueEvenOrOdd:          ;continuation after conditional logic
+  call _print
 
   leave
   ret
 
-even:
-  
-  
+_print:                     ;print function with msg pointer and msg len as parameters
+  push ebp
+  mov ebp, esp
+  sub esp, 8
+  mov eax, DWORD[ebp+12]    ;initializing stack frame
+  mov DWORD[ebp-4], eax
+  mov eax, DWORD[ebp+8]
+  mov DWORD[ebp-8], eax
+
+  mov eax, 4                ;sys_write
+  mov ebx, 1
+  mov ecx, DWORD[ebp-4]     ;pointer to msg
+  mov edx, DWORD[ebp-8]     ;value of msgLen
+  int 0x80
+
+  leave
+  ret
 
 section .data
   num dd 27
 
-  msg db "Number "
+  msgEven db "The number is even.", 0xA
+  evenLen equ $ - msgEven
+  msgOdd db "The number is odd.", 0xA
+  oddLen equ $ - msgOdd
