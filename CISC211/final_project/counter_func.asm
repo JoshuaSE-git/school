9c62634a1d6527c9bd4f24f598f933de4e879f15
Joshua Emralino <joshua.s.emralino@gmail.com>
2025-05-25 14:20:10 -0700

    211 final

--- CISC211/final_project/counter_func.asm
+++ CISC211/final_project/counter_func.asm
@@ -2,88 +2,88 @@ section .text
   global _start
 
 _start:
-  push DWORD[counter]
-  call _counter
+  push DWORD[counter]                   ;push counter value as argument to _counter
+  call _counter                         ;call _counter func
 
   mov eax, 1
   int 0x80
 
-_counter:  ;counter function
-  push ebp
+_counter:                               ;counter function
+  push ebp                              ;initialize stack frame
   mov ebp, esp
   sub esp, 4
   mov eax, [ebp+8]
-  mov [ebp-4], eax   ;counter num
+  mov [ebp-4], eax                      ;counter variable in stack
   
-  mov [currentCounter], eax
+  mov [currentCounter], eax             ;initialize counter (starts at 20,000)
 
-  mov eax, 8
+  mov eax, 8                            ;create new file and get file descriptor
   mov ebx, filename
   mov ecx, 0777o
   int 0x80
 
-  mov [fd], eax
+  mov [fd], eax                         ;move file descriptor to fd for easy access
 
   jmp outputLoop
 
-outputLoop:  ;outer loop for writing counter to file
-  mov eax, [currentCounter]
-  xor ecx, ecx
-  jmp pushLoop
+outputLoop:                             ;outer loop for writing counter to file
+  mov eax, [currentCounter]             ;move current counter val to eax
+  xor ecx, ecx                          ;clear ecx
+  jmp pushLoop                          ;jump to inner pushLoop
 
-pushLoop:  ;pushing individual digits to stack (inner loop)
+pushLoop:                               ;pushing individual digits to stack (inner loop)
   mov ebx, 10
   xor edx, edx
   div ebx
   push edx
-  inc ecx                  ;ecx stores our counter number length
+  inc ecx                               ;ecx stores length of current number
   cmp eax, 9
-  jg pushLoop
+  jg pushLoop                           ;loop to push all digits to stack
   push eax
   inc ecx
-  mov [printVarLen], ecx   ;move counter number length to printVarLen
-  jmp writeLoop            ;after digits are done being pushed, we go to writeLoop
+  mov [printVarLen], ecx                ;move current number length to printVarLen
+  jmp writeLoop                         ;after digits are done being pushed, we go to writeLoop
 
-writeLoop:  ;writing individual digits to file (inner loop)
+writeLoop:                              ;writing individual digits to file (inner loop)
   pop eax
   add eax, 48
-  mov [printVar], eax      ;printVar now has ascii converted digit
+  mov [printVar], eax                   ;printVar now has ascii converted digit
 
-  mov eax, 4
+  mov eax, 4                            ;writing individual digit to output file
   mov ebx, [fd]
   mov ecx, printVar
   mov edx, 1
   int 0x80
 
-  mov ecx, [printVarLen]   ;handle loop logic based on varLen
+  mov ecx, [printVarLen]                ;handle loop logic based on varLen (number length)
   dec ecx
   mov [printVarLen], ecx
-  cmp ecx, 0               ;if there are still more digits, we loop
+  cmp ecx, 0                            ;if there are still more digits, we loop
   jg writeLoop 
 
-  mov eax, 4               ;after digits are done, we write a new line    
+  mov eax, 4                            ;after digits are written, we write a new line
   mov ebx, [fd]
   mov ecx, newLine     
   mov edx, 1
   int 0x80
 
-  jmp continueOutputLoop    
+  jmp continueOutputLoop                ;inner loops are done, jump back to outer loop logic
 
-continueOutputLoop:  ;continue outer loop logic
+continueOutputLoop:                     ;continue outer loop logic
   mov eax, [currentCounter]
   dec eax
   mov [currentCounter], eax
-  cmp eax, 0
-  jge outputLoop
-  jmp continueCounterFunc
+  cmp eax, 0                            ;checks if current counter value is greater than 0
+  jge outputLoop                        ;if >= 0, continue outer outputLoop
+  jmp continueCounterFunc               ;if < 0, counter is done, now move to ending logic
 
 continueCounterFunc:
-  mov eax, 6           ;close file 
+  mov eax, 6                            ;close file
   mov ebx, [fd]
   int 0x80
   
-  leave
-  ret
+  leave                                 ;clean-up stack
+  ret                                   ;return to _start
 
 section .bss
   fd resd 1
